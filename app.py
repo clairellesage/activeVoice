555d5ded88334a9fbc24c426712ee90886d09872
Claire Lesage <clairelesage@fb.com>
2020-06-19 18:12:46 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import sys
+
+def newsAPI(keyword: str):
+
+	url = ('http://newsapi.org/v2/everything?'
+	       f'q={keyword}&'
+	       'from=2020-06-18&'
+	       'sortBy=relevancy&'
+	       'apiKey=53eefc1a132347b0acae957ce1a74e0b')
+
+	response = requests.get(url)
+
+	return response.json()
+
+def parseJSON(json: object):
+	# print(json['articles'][1])
+	articles = json['articles']
+	stats = []
+	for article in articles:
+		artObj = {article['source']['name'] : article['title'], "contents" : article['description']}
+		stats.append(artObj)	
+	return artObj
+
+def main():
+	keyword = sys.argv[1]
+	json = newsAPI(keyword)
+	print(parseJSON(json))
+
+main()
